505cb262edcafc97dd1a9e0bc3717d1c9a8114bd
canusorn <anusorn1998@gmail.com>
2022-10-06 18:18:50 +0700

    add tm1637 display

--- ntc_temp/ntc_temp.ino
+++ ntc_temp/ntc_temp.ino
@@ -1,7 +1,13 @@
-
-
+#include <TM1637.h>
+TM1637 tm(D1, D2);
 void setup() {
   Serial.begin(115200);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  tm.begin();
+  tm.setBrightness(4);
 }
 
 void loop() {
@@ -11,5 +17,6 @@ void loop() {
   float  temp = (1 / ((ln / 3950) + (1 / (25 + 273.15))));
   temp -= 273.15;
   Serial.println(temp);
+  tm.display(String(temp,0) + " C");
   delay(200);
 }
