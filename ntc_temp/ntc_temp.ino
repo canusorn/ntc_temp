3a55f9b0d3741896e1ee9c16723ec19da86919fc
canusorn <anusorn1998@gmail.com>
2022-10-07 13:26:00 +0700

    Update ntc_temp.ino

--- ntc_temp/ntc_temp.ino
+++ ntc_temp/ntc_temp.ino
@@ -1,18 +1,20 @@
 #include <TM1637.h>
-TM1637 tm(D1, D2);
+
+TM1637 tm(D0, D5);
+
 void setup() {
   Serial.begin(115200);
-  digitalWrite(D3, HIGH);
-  digitalWrite(D4, LOW);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
+  digitalWrite(D6, HIGH);
+  digitalWrite(D7, LOW);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
   tm.begin();
   tm.setBrightness(4);
 }
 
 void loop() {
   int Vt = analogRead(A0);
-  float Rt = (float)(1023 * 10000) / Vt - 10000;
+  float Rt = (float)(1023 * 9900) / Vt - 9900;
   float ln = log(Rt / 10000);
   float  temp = (1 / ((ln / 3950) + (1 / (25 + 273.15))));
   temp -= 273.15;
